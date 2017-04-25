3ac0433b3eae7917f264ca2b9ed609eb6a685fea
党伟 <dangwei@yonyou.com>
2017-04-25 14:50:47 +0800

    去除空格

--- src/Formula.js
+++ src/Formula.js
@@ -157,7 +157,7 @@ export default class Formula extends React.Component{
         let _refText = this.props.refText;
         let _refItem = this.props.refItem;
         let defaultSelected = this.props.refSelected ? Object.assign([], [this.props.refSelected]) : [];
-		const filterByFields = ['name', 'code'];
+        const filterByFields = ['name', 'code'];
 		
         return (
             <Modal show={_this.state.showModal} onHide={_this.close} className="static-modal">
@@ -183,7 +183,7 @@ export default class Formula extends React.Component{
                                     ref={_refItem}
                                     defaultSelected={defaultSelected}
                                     renderMenuItemChildren={_this.renderMenuItemChildren}
-									filterBy={filterByFields}
+				    filterBy={filterByFields}
                                 />
                             </div>
                          </Tab>
